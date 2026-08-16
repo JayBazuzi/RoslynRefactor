a18cb6c8a4940befbd1428971995c7cc354af3b0
Jay Bazuzi <jay@bazuzi.com>
2026-08-16 03:16:59 +0000

    . t fix path on unixlike

--- Tests/ProcessTestHost.cs
+++ Tests/ProcessTestHost.cs
@@ -11,7 +11,7 @@ static class ProcessTestHost
 {
     // The ProjectReference to RoslynRefactor.csproj guarantees this assembly is built and
     // available alongside the test assembly; using its location avoids hardcoding a path.
-    static readonly string ToolDllPath = typeof(RoslynRefactor.ICommand).Assembly.Location.Replace(".dll", ".exe");
+    static readonly string ToolDllPath = typeof(RoslynRefactor.ICommand).Assembly.Location.Replace(".dll", OperatingSystem.IsWindows() ? ".exe" : "");
 
     static readonly string FixturesSourceDir = FindFixturesSourceDir();
 
